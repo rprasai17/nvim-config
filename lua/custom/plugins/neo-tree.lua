8d6303c4486e15d264248d546379ef28e8838bca
Rupan Prasai <rupanprasai18@gmail.com>
2025-09-09 20:38:57 -0500

    updated neo-tree configs

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -10,12 +10,46 @@ return {
   ---@module 'neo-tree'
   ---@type neotree.Config
   opts = {
-    -- options go here
     enable_git_status = true,
     open_files_using_relative_paths = false,
     default_component_configs = {
+      indent = {
+        indent_size = 2,
+        padding = 1, -- extra padding on left hand side
+        -- indent guides
+        with_markers = true,
+        indent_marker = '│',
+        last_indent_marker = '└',
+        highlight = 'NeoTreeIndentMarker',
+        -- expander config, needed for nesting files
+        with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+        expander_collapsed = '',
+        expander_expanded = '',
+        expander_highlight = 'NeoTreeExpander',
+      },
+      icon = {
+        folder_closed = '',
+        folder_open = '',
+        folder_empty = '󰜌',
+        provider = function(icon, node, state) -- default icon provider utilizes nvim-web-devicons if available
+          if node.type == 'file' or node.type == 'terminal' then
+            local success, web_devicons = pcall(require, 'nvim-web-devicons')
+            local name = node.type == 'terminal' and 'terminal' or node.name
+            if success then
+              local devicon, hl = web_devicons.get_icon(name)
+              icon.text = devicon or icon.text
+              icon.highlight = hl or icon.highlight
+            end
+          end
+        end,
+        -- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
+        -- then these will never be used.
+        default = '*',
+        highlight = 'NeoTreeFileIcon',
+      },
       name = {
         trailing_slash = true,
+        highlight = 'NeoTreeFileName',
       },
     },
     window = {
@@ -39,6 +73,14 @@ return {
       group_empty_dirs = false,
       use_libuv_file_watcher = true,
     },
+    buffers = {
+      follow_current_file = {
+        enabled = true,
+        leave_dirs_open = true,
+      },
+      group_empty_dirs = true,
+    },
+
     --
     --
     --
@@ -48,8 +90,15 @@ return {
   init = function()
     vim.api.nvim_create_autocmd('VimEnter', {
       callback = function()
-        vim.cmd 'Neotree'
-        vim.cmd 'wincmd l'
+        -- defer to ensure neo-tree is loaded
+        vim.schedule(function()
+          require('neo-tree.command').execute {
+            action = 'show', -- or "show"
+            reveal_force_cwd = true,
+          }
+          -- optional: move focus back to the right-hand window
+          vim.cmd 'wincmd l'
+        end)
       end,
     })
   end,
