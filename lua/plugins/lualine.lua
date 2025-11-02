33e7e66d1975139ff314f37e3e9fde2dddf1669f
Rupan Prasai <rupanprasai18@gmail.com>
2025-11-01 20:37:35 -0500

    updating

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -10,28 +10,7 @@ return {
         always_show_tabline = true,
       },
       tabline = {
-        lualine_a = {
-          {
-            function()
-              local grapple = require 'grapple'
-              if not grapple.exists() then
-                return ''
-              end
-              local tag = grapple.find()
-              if not tag then
-                return ''
-              end
-
-              local index = grapple.name_or_index()
-              local parent_dir = vim.fn.fnamemodify(tag.path, ':h:t')
-              local file = vim.fn.fnamemodify(tag.path, ':t')
-              return '%#lualine_a_normal#' .. string.format('%s: %s/%s', index, parent_dir, file) .. '%*'
-            end,
-            cond = function()
-              return package.loaded['grapple'] and require('grapple').exists()
-            end,
-          },
-        },
+        lualine_a = {},
         lualine_b = {},
         lualine_c = {},
         lualine_x = { require('grapple-line').lualine },
